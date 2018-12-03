9f336e2289ae885372b2f5b8f84bfd1696ccdf4f
laurence <laurencehall1@outlook.com>
2018-12-03 18:43:39 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css'
+class App extends Component {
+  state ={
+   
+  }
+   
+  render() {
+  return(
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+  }
+}
+
+export default App;
