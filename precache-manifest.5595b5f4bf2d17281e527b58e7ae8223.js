e71dead6e66ff2679d2d64b0b138fb945d6576ed
laurence <laurencehall1@outlook.com>
2018-12-05 17:25:30 -0500

    Updates

---
+++ precache-manifest.5595b5f4bf2d17281e527b58e7ae8223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97d61c26c34b3d564f98",
+    "url": "/React-Multi-Step-Form/static/js/runtime~main.97d61c26.js"
+  },
+  {
+    "revision": "247df868b6178465cbed",
+    "url": "/React-Multi-Step-Form/static/js/main.247df868.chunk.js"
+  },
+  {
+    "revision": "e130c10b169b6631f3da",
+    "url": "/React-Multi-Step-Form/static/js/1.e130c10b.chunk.js"
+  },
+  {
+    "revision": "247df868b6178465cbed",
+    "url": "/React-Multi-Step-Form/static/css/main.9873f146.chunk.css"
+  },
+  {
+    "revision": "97e134004a67e0cf548fddc12ba6fbf1",
+    "url": "/React-Multi-Step-Form/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-Multi-Step-Form/precache-manifest.5595b5f4bf2d17281e527b58e7ae8223.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-Multi-Step-Form/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
